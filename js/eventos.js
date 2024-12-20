d211e7ed3d3e50ea623c54abee1429a83ce167c9
unknown <eliascunha0630@digitalcollege.internal>
2024-12-19 21:42:09 -0300

    aprendendo javascript

---
+++ js/eventos.js
@@ -0,0 +1,38 @@
+function enviar() {
+    event.preventDefault();
+    
+    let input_nome = document.getElementById('nome');
+    let input_descricao = document.getElementById('descricao');
+    if (input_nome.value == '') {
+        input_nome.style.border = '1px solid red';
+        document.getElementById('erro_nome').style.display = 'block' ;
+
+    } else{
+        input_nome.style.border = '1px solid green';
+        document.getElementById('erro_nome').style.display = 'none' ;
+
+    }
+
+
+    
+
+    
+    if (input_descricao.value == '') {
+        input_descricao.style.border = '1px solid red';
+        document.getElementById('erro_descricao').style.display = 'block' ;
+    } else {
+        input_descricao.style.border = '1px solid green';
+        document.getElementById('erro_descricao').style.display = 'none' ;
+    }
+}
+
+
+
+
+
+
+// let resposta = prompt('Quer mostrar as mensagens de erro?');
+
+// if (resposta == 'sim')  {
+//    document.getElementById('erro_nome').style.display = 'block' ;
+ // }
